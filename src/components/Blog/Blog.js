e05d913a7aaf30b75d9239cd4011d602ae8940f8
Byungchan Lim <bl458@cornell.edu>
2020-08-15 19:46:07 +0900

    feat: Add placeholder nav

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { bubble as Menu } from "react-burger-menu";
+
 import "../styles.css";
 
-const Blog = () => (
-  <div className="container">
-    <h1>Hello World</h1>
-  </div>
-);
+const Blog = () => {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <Menu>
+          <a id="home" className="menu-item" href="/">
+            Home
+          </a>
+          <a id="about" className="menu-item" href="/about">
+            About
+          </a>
+          <a id="contact" className="menu-item" href="/contact">
+            Contact
+          </a>
+        </Menu>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
