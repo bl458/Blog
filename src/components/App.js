6b398088d864a4dec8936a59cb818975074fcb7b
Byungchan Lim <bl458@cornell.edu>
2020-08-15 19:12:06 +0900

    feat: Set up file structure

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Blog from "./Blog/Blog";
+
+const App = () => <Blog />;
+
+export default App;
---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles.css";
+
+const Blog = () => (
+  <div className="container">
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default Blog;
