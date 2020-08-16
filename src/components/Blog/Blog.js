5f13b2be51cc3545db2485b371d93fb870bf692c
Byungchan Lim <bl458@cornell.edu>
2020-08-16 16:50:37 +0900

    feat: Handle changing window size

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { push as Menu } from "react-burger-menu";
 
 import "../styles.css";
@@ -13,18 +13,48 @@ const Container = styled.div`
 
 const MainContainer = styled.div`
   text-align: center;
-  width: 100vw-300;
   align-self: flex-end;
+  width: ${(props) => (props.menuOpen ? "calc((100vw-)/100vw*100)" : "100%")};
+  background-color: red;
 `;
 
 const Title = styled.h1`
   margin-top: 0;
 `;
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
 const Blog = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  const handleMenuChange = (state) => setMenuOpen(state.isOpen);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Container>
-      <Menu noOverlay isOpen pageWrapId={"page-wrap"}>
+      <Menu
+        noOverlay
+        isOpen
+        pageWrapId={"page-wrap"}
+        onStateChange={handleMenuChange}
+      >
         <a id="home" href="/">
           Home
         </a>
@@ -36,7 +66,7 @@ const Blog = () => {
         </a>
       </Menu>
 
-      <MainContainer id="page-wrap">
+      <MainContainer id="page-wrap" menuOpen={menuOpen}>
         <Title>Title</Title>
       </MainContainer>
     </Container>
