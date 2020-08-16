90224a83f16cf573fd92ad5ef99f03565067db8c
Byungchan Lim <bl458@cornell.edu>
2020-08-16 16:29:24 +0900

    refactor: Migrate css to styled-component

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,16 +2,29 @@ import React from "react";
 import { push as Menu } from "react-burger-menu";
 
 import "../styles.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+`;
+
+const MainContainer = styled.div`
+  text-align: center;
+  width: 100vw-300;
+  align-self: flex-end;
+`;
+
+const Title = styled.h1`
+  margin-top: 0;
+`;
 
 const Blog = () => {
   return (
-    <div className="container">
-      <Menu
-        noOverlay
-        isOpen
-        pageWrapId={"page-wrap"}
-        outerContainerId={"container"}
-      >
+    <Container>
+      <Menu noOverlay isOpen pageWrapId={"page-wrap"}>
         <a id="home" href="/">
           Home
         </a>
@@ -23,11 +36,10 @@ const Blog = () => {
         </a>
       </Menu>
 
-      <div id="page-wrap" className="main">
-        <div className="header"></div>
-        <h1 className="title">Title</h1>
-      </div>
-    </div>
+      <MainContainer id="page-wrap">
+        <Title>Title</Title>
+      </MainContainer>
+    </Container>
   );
 };
 
